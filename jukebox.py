e0c10ad35e4dc56c5ce2de86cc3403d48b4ea053
etiandre <eti.andre@gmail.com>
2017-05-22 16:39:50 +0200

    Faisage marcher du truc de recherche

--- jukebox.py
+++ jukebox.py
@@ -1,14 +1,16 @@
 import os
 from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash
-from IPython import embed
 app = Flask(__name__)
 
 #import playpause
 import search
 #import admin
 #import vote
-import playlist
+#import playlist
+
+@app.route("/")
+def accueil():
+    return render_template("accueil.html")
 
 if __name__ == "__main__":
-    embed()
     search.app.run(host='0.0.0.0', port=8080)
