f22cca4eac66b6acd9591ed8f79df9e29bd5e6e6
⛧ <matthias.goffette@telecom-sudparis.eu>
2019-03-22 18:18:42 +0100

    Fix bug with jamendo

--- jukebox/__init__.py
+++ jukebox/__init__.py
@@ -62,7 +62,7 @@ def player_worker():
     while len(app.playlist) > 0:
         print("playing {}".format(app.playlist[0]))
         player = None
-        if app.playlist[0]["source"] in ["youtube", "bandcamp", "soundcloud"]:
+        if app.playlist[0]["source"] in ["youtube", "bandcamp", "soundcloud", "jamendo"]:
             app.mpv = MyMPV(app.playlist[0]["url"], ["--no-video"])
         while not app.mpv.finished():
             time.sleep(0.5)
