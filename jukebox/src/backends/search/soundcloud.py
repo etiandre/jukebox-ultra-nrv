8ce5bdda076e9c650cc4e4af1022e543e8396186
⛧ <matthias.goffette@telecom-sudparis.eu>
2019-03-26 18:02:18 +0100

    Makes soundcloud searches faster
      * Does not modify the search with `scsearch:`

--- jukebox/src/backends/search/soundcloud.py
+++ jukebox/src/backends/search/soundcloud.py
@@ -5,25 +5,17 @@ from flask import session
 import youtube_dl
 import json
 
-ydl_opts = {
-        'writeinfojson': True,
-        'skip_download': True, # we do want only a json file
-        'outtmpl': "tmp_music_%(playlist_index)s", # the json is tmp_music.info.json
-        }
 
 def search(query):
+    ydl_opts = {
+        'skip_download': True, # we do want only a json file
+        }
     results = []
 
     # We use youtube-dl to get the song metadata
     # only problem : it's a bit slow (about 3 seconds)
     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-        ydl.download([query])
-
-    with open("tmp_music_1.info.json", 'r') as f:
-        metadata = f.read()
-        metadata = json.loads(metadata)
-        print(type(metadata))
-
+        metadata = ydl.extract_info(query, False)
 
     results.append({
         "source": "soundcloud",
@@ -37,6 +29,11 @@ def search(query):
     return results
 
 def search_engine(query):
+    ydl_opts = {
+            'writeinfojson': True,
+            'skip_download': True, # we do want only a json file
+            'outtmpl': "tmp_music_%(playlist_index)s", # the json is tmp_music.info.json
+            }
     results = []
 
     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
