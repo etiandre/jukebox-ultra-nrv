3b1260aed170e0e821dd4395c98d417741312f8c
⛧ <matthias.goffette@telecom-sudparis.eu>
2019-03-29 12:04:08 +0100

    Adds support of Bandcamp "playlists"
      * Playlists may be albums, artists with only one release and maybe
      more

--- jukebox/src/backends/search/bandcamp.py
+++ jukebox/src/backends/search/bandcamp.py
@@ -17,14 +17,30 @@ def search(query):
     results = []
     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
         json_info = ydl.extract_info(query, False)
-    print(json.dumps(json_info))
-    results.append({
-        "source": "bandcamp",
-        "title": json_info["track"],
-        "artist": json_info["artist"],
-        "url": query,
-        "albumart_url": json_info["thumbnail"],
-        "duration": int(json_info["duration"]),
-        "id": json_info["id"]
-        })
+    #print(json.dumps(json_info))
+
+    # If we have a playlist
+    if "_type" in json_info and json_info["_type"] == "playlist":
+        for res in json_info["entries"]:
+            results.append({
+                "source": "bandcamp",
+                "title": res["track"],
+                "artist": res["artist"],
+                "url": res["webpage_url"],
+                "albumart_url": res["thumbnails"][0]["url"],
+                "duration": int(res["duration"]),
+                "id": res["id"]
+                })
+
+    # It's a single music
+    else:
+        results.append({
+            "source": "bandcamp",
+            "title": json_info["track"],
+            "artist": json_info["artist"],
+            "url": query,
+            "albumart_url": json_info["thumbnail"],
+            "duration": int(json_info["duration"]),
+            "id": json_info["id"]
+            })
     return results
